2a075b92016c31200a460b7089c44506a39c0a35
Наташа Самойленко <pyneng.course@gmail.com>
2020-02-20 10:50:11 +0200

    Подправила тест 20.1, чтобы порядок в списках был не важен

--- exercises/20_concurrent_connections/tests/test_task_20_1.py
+++ exercises/20_concurrent_connections/tests/test_task_20_1.py
@@ -14,11 +14,17 @@ def test_functions_created():
 def test_function_return_value():
     list_of_ips = ["1.1.1", "8.8.8.8", "8.8.4.4", "8.8.7.1"]
     correct_return_value = get_reach_unreach(list_of_ips)
+    correct_reachable, correct_unreachable = correct_return_value
     return_value = task_20_1.ping_ip_addresses(list_of_ips)
+    return_reachable, return_unreachable = return_value
     assert return_value != None, "Функция ничего не возвращает"
     assert type(return_value) == tuple and all(
         type(item) == list for item in return_value
     ), "Функция должна возвращать кортеж с двумя списками"
     assert (
-        return_value == correct_return_value
+        sorted(return_reachable) == sorted(correct_reachable)
     ), "Функция возвращает неправильное значение"
+    assert (
+        sorted(return_unreachable) == sorted(correct_unreachable)
+    ), "Функция возвращает неправильное значение"
+
