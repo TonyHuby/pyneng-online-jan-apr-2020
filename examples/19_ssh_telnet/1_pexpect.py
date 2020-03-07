9c231817ed44c2ad0f738f09cacaeacf377f828e
pyneng <pyneng.course@gmail.com>
2020-03-07 12:01:50 +0000

    lecture 12

--- examples/19_ssh_telnet/1_pexpect.py
+++ examples/19_ssh_telnet/1_pexpect.py
@@ -1,80 +1,39 @@
 import pexpect
-import getpass
-import sys
+from getpass import getpass
 
-command = sys.argv[1]
-user = input("Username: ")
-password = getpass.getpass()
-enable_pass = getpass.getpass(prompt="Enter enable password: ")
 
-devices_ip = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
-
-for ip in devices_ip:
-    print("Connection to device {}".format(ip))
-    with pexpect.spawn("ssh {}@{}".format(user, ip)) as ssh:
-
-        ssh.expect("Password:")
+def send_command_cisco(ip, username, password, enable, commands):
+    with pexpect.spawn(f"ssh {username}@{ip}") as ssh:
+        ssh.expect("Password")
         ssh.sendline(password)
+        ssh.expect(">")
 
-        ssh.expect("[#>]")
         ssh.sendline("enable")
+        ssh.expect("Password")
 
-        ssh.expect("Password:")
-        ssh.sendline(enable_pass)
-
+        ssh.sendline(enable)
         ssh.expect("#")
+
         ssh.sendline("terminal length 0")
-
         ssh.expect("#")
-        ssh.sendline(command)
 
-        ssh.expect("#")
-        print(ssh.before.decode("ascii"))
-"""
-Example:
+        result = []
+        for command in commands:
+            ssh.sendline(command)
+            returncode = ssh.expect(["#", pexpect.TIMEOUT, pexpect.EOF])
+            if returncode == 0:
+                output = ssh.before.decode("utf-8")
+                result.append(output)
+            else:
+                print("Возникла ошибка")
+        return result
 
-$ python 1_pexpect.py 'sh ip int br'
-Username: nata
-Password:
-Enter enable secret:
-Connection to device 192.168.100.1
-sh ip int br
-Interface              IP-Address      OK? Method Status                Protocol
-FastEthernet0/0        192.168.100.1   YES NVRAM  up                    up
-FastEthernet0/1        unassigned      YES NVRAM  up                    up
-FastEthernet0/1.10     10.1.10.1       YES manual up                    up
-FastEthernet0/1.20     10.1.20.1       YES manual up                    up
-FastEthernet0/1.30     10.1.30.1       YES manual up                    up
-FastEthernet0/1.40     10.1.40.1       YES manual up                    up
-FastEthernet0/1.50     10.1.50.1       YES manual up                    up
-FastEthernet0/1.60     10.1.60.1       YES manual up                    up
-FastEthernet0/1.70     10.1.70.1       YES manual up                    up
-R1
-Connection to device 192.168.100.2
-sh ip int br
-Interface              IP-Address      OK? Method Status                Protocol
-FastEthernet0/0        192.168.100.2   YES NVRAM  up                    up
-FastEthernet0/1        unassigned      YES NVRAM  up                    up
-FastEthernet0/1.10     10.2.10.1       YES manual up                    up
-FastEthernet0/1.20     10.2.20.1       YES manual up                    up
-FastEthernet0/1.30     10.2.30.1       YES manual up                    up
-FastEthernet0/1.40     10.2.40.1       YES manual up                    up
-FastEthernet0/1.50     10.2.50.1       YES manual up                    up
-FastEthernet0/1.60     10.2.60.1       YES manual up                    up
-FastEthernet0/1.70     10.2.70.1       YES manual up                    up
-R2
-Connection to device 192.168.100.3
-sh ip int br
-Interface              IP-Address      OK? Method Status                Protocol
-FastEthernet0/0        192.168.100.3   YES NVRAM  up                    up
-FastEthernet0/1        unassigned      YES NVRAM  up                    up
-FastEthernet0/1.10     10.3.10.1       YES manual up                    up
-FastEthernet0/1.20     10.3.20.1       YES manual up                    up
-FastEthernet0/1.30     10.3.30.1       YES manual up                    up
-FastEthernet0/1.40     10.3.40.1       YES manual up                    up
-FastEthernet0/1.50     10.3.50.1       YES manual up                    up
-FastEthernet0/1.60     10.3.60.1       YES manual up                    up
-FastEthernet0/1.70     10.3.70.1       YES manual up                    up
-R3
 
-"""
+if __name__ == "__main__":
+    command = ["sh clock", "sh ip int br", "sh ip arp"]
+    username = input("Введите имя пользователя: ")
+    password = getpass()
+    enable = getpass("Введи пароль режима enable" )
+    ip_list = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
+    for ip in ip_list:
+        print(send_command_cisco(ip, username, password, enable, command))
