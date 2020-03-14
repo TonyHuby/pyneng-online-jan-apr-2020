6d1f9265ee36c03494d0403f6d948d2f3a03bbc5
pyneng <pyneng.course@gmail.com>
2020-03-14 13:33:29 +0000

    Обновила тест 19.2a

--- exercises/19_ssh_telnet/tests/test_task_19_2a.py
+++ exercises/19_ssh_telnet/tests/test_task_19_2a.py
@@ -14,7 +14,11 @@ def test_functions_created():
 def test_function_return_value(
     capsys, r1_test_connection, first_router_from_devices_yaml
 ):
-    test_commands = ["log 10.255.255.1", "log buffered 20010", "no log console"]
+    test_commands = [
+        "logging 10.255.255.1",
+        "logging buffered 20010",
+        "no logging console",
+    ]
     correct_return_value = r1_test_connection.send_config_set(test_commands)
     return_value = task_19_2a.send_config_commands(
         first_router_from_devices_yaml, test_commands
